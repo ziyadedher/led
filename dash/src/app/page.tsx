2b558f073ea22574efecb2b5d7dd4b5893759f09
Ziyad Edher <ziyadedher@asana.com>
2023-11-06 19:20:50 -0800

    Clean up UI a bit

--- dash/src/app/page.tsx
+++ dash/src/app/page.tsx
@@ -1,8 +1,11 @@
 "use client";
 
 import cx from "classnames";
-import { useState, useMemo } from "react";
+import { Button, Checkbox, Label, Tabs, Tooltip } from "flowbite-react";
+import { useState, useMemo, useEffect } from "react";
 import { CirclePicker } from "react-color";
+import { HiSwatch } from "react-icons/hi2";
+import { PiRainbowBold } from "react-icons/pi";
 import {
   CheckCircleIcon,
   ExclamationCircleIcon,
@@ -11,6 +14,7 @@ import {
 
 import { addEntry, clearEntries } from "@/app/actions";
 
+const SHORT_LENGTH = 12;
 const MAX_LENGTH = 64;
 
 const hexToRgb = (hex: string): { r: number; g: number; b: number } => {
@@ -44,19 +48,29 @@ export default function RootPage() {
     () => isValid && text.length > 0,
     [isValid, text]
   );
+  const isLong = useMemo(() => text.length > SHORT_LENGTH, [text]);
+  useEffect(() => {
+    if (isLong) {
+      setMarqueeSpeed(5);
+    }
+  }, [isLong]);
 
-  const [colorMode, setColorMode] = useState("Color");
+  const [colorMode, setColorMode] = useState(0);
   const [color, setColor] = useState("ffffff");
   const [isRainbowPerLetter, setIsRainbowPerLetter] = useState(false);
   const [rainbowSpeed, setRainbowSpeed] = useState(5);
 
   const [marqueeSpeed, setMarqueeSpeed] = useState(5);
-  const [twinkleSpeed, setTwinkleSpeed] = useState(5);
 
   return (
-    <div className="h-full p-8 flex flex-col gap-12 justify-center items-center">
+    <div className="relative h-full p-8 flex flex-col gap-12 justify-center items-center">
       <div className="w-full max-w-2xl flex flex-col gap-8">
-        <h1 className="text-4xl text-center">c&apos;mon, write something</h1>
+        <div className="flex flex-col gap-1">
+          <h1 className="text-4xl text-center">c&apos;mon, write something</h1>
+          <h2 className="text-xs text-center text-gray-400">
+            and maybe sign your name too
+          </h2>
+        </div>
         <form
           className="flex flex-col gap-8 items-center"
           onSubmit={(e) => {
@@ -68,7 +82,7 @@ export default function RootPage() {
               text,
               options: {
                 color:
-                  colorMode === "Color"
+                  colorMode === 0
                     ? { Rgb: hexToRgb(color) }
                     : {
                         Rainbow: {
@@ -126,9 +140,9 @@ export default function RootPage() {
             <p className="text-xs">
               {isValid ? (
                 isSubmitable ? (
-                  <span className="text-gray-500">Hit enter to submit...</span>
+                  <span className="text-gray-400">Hit enter to submit...</span>
                 ) : (
-                  <span className="text-gray-500">Use the text box.</span>
+                  <span className="text-gray-400">Use the text box.</span>
                 )
               ) : (
                 <span className="text-red-500">
@@ -140,115 +154,99 @@ export default function RootPage() {
         </form>
       </div>
 
-      <div className="w-full max-w-md flex flex-col gap-4 items-center">
-        <div className="w-full border-b border-gray-200">
-          <nav className="w-full -mb-px flex">
-            {["Color", "Rainbow"].map((tab) => (
-              <button
-                key={tab}
-                type="button"
-                onClick={() => {
-                  setColorMode(tab);
-                }}
-                className={cx(
-                  tab === colorMode
-                    ? "border-blue-500 text-blue-600"
-                    : "border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700",
-                  "w-1/2 border-b-2 py-4 px-1 text-center text-sm font-medium"
-                )}
-              >
-                {tab}
-              </button>
-            ))}
-          </nav>
-        </div>
-        <div className="h-48 pt-2">
-          {colorMode === "Color" ? (
-            <div className="flex flex-col gap-4 items-center">
-              {/* <h2 className="text-xs text-gray-500">Pick a color, any color</h2> */}
-              <div className="relative mt-2 rounded-md shadow-sm bg-white overflow-hidden">
-                <div className="pointer-events-none absolute inset-y-0 left-0 flex items-center p-3 bg-gray-100">
-                  <span className="text-md text-gray-400">#</span>
-                </div>
-                <input
-                  type="text"
-                  name="price"
-                  value={color}
-                  onChange={(e) => setColor(e.target.value)}
-                  className="block relative z-10 w-full rounded-md border-0 py-1.5 px-12 bg-transparent text-sm leading-6 text-gray-900 ring-1 ring-inset ring-gray-200 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-600"
-                  placeholder="ffffff"
-                />
-                <div
-                  className="pointer-events-none absolute inset-y-0 right-0 flex p-4 items-center"
-                  style={{ backgroundColor: `#${color}` }}
-                />
+      <Tabs.Group
+        onActiveTabChange={(tab) => setColorMode(tab)}
+        style="underline"
+        className="w-full max-w-lg justify-center"
+      >
+        <Tabs.Item active title="Color" icon={HiSwatch}>
+          <div className="h-48 flex flex-col gap-4 items-center">
+            <div className="relative mt-2 rounded-md shadow-sm bg-white overflow-hidden">
+              <div className="pointer-events-none absolute inset-y-0 left-0 flex items-center p-3 bg-gray-100">
+                <span className="text-md text-gray-400">#</span>
               </div>
-              <CirclePicker
-                color={color}
-                onChange={(c) => {
-                  setColor(c.hex.slice(1));
-                }}
+              <input
+                type="text"
+                name="price"
+                value={color}
+                onChange={(e) => setColor(e.target.value)}
+                className="block relative z-10 w-full rounded-md border-0 py-1.5 px-12 bg-transparent text-sm leading-6 text-gray-900 ring-1 ring-inset ring-gray-200 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-600"
+                placeholder="ffffff"
+              />
+              <div
+                className="pointer-events-none absolute inset-y-0 right-0 flex p-4 items-center"
+                style={{ backgroundColor: `#${color}` }}
               />
             </div>
-          ) : (
-            <fieldset className="flex flex-col gap-4">
-              <div className="relative flex items-start">
-                <div className="flex h-6 items-center">
-                  <input
-                    name="comments"
-                    type="checkbox"
-                    checked={isRainbowPerLetter}
-                    onChange={(e) => setIsRainbowPerLetter(e.target.checked)}
-                    className="h-4 w-4 rounded border-gray-300 text-blue-600 focus:ring-blue-600"
-                  />
-                </div>
-                <div className="ml-3 text-sm leading-6">
-                  <span className="font-medium text-gray-900">
-                    Rainbow per letter
-                  </span>{" "}
-                  <span className="text-gray-500">
-                    makes each letter glow a different color.
-                  </span>
-                </div>
-              </div>
-            </fieldset>
-          )}
-        </div>
-      </div>
+            <CirclePicker
+              color={color}
+              onChange={(c) => {
+                setColor(c.hex.slice(1));
+              }}
+            />
+          </div>
+        </Tabs.Item>
+        <Tabs.Item title="Rainbow" icon={PiRainbowBold}>
+          <fieldset className="h-48 flex flex-col gap-4">
+            <div className="flex flex-row items-center gap-2">
+              <Checkbox
+                id="rainbow-per-letter"
+                checked={isRainbowPerLetter}
+                onChange={(e) => setIsRainbowPerLetter(e.target.checked)}
+                className="cursor-pointer"
+              />
+              <Label htmlFor="rainbow-per-letter" className="cursor-pointer">
+                <span className="font-medium text-gray-900">
+                  Rainbow per letter
+                </span>{" "}
+                <span className="text-gray-500">
+                  makes each letter glow a different color.
+                </span>
+              </Label>
+            </div>
+          </fieldset>
+        </Tabs.Item>
+      </Tabs.Group>
 
       <div className="flex flex-col gap-4 items-center">
         <h2 className="text-xs text-gray-500 border-b border-gray-300 pb-2">
           Make it special
         </h2>
         <fieldset className="flex flex-col gap-4">
-          <div className="relative flex items-start">
-            <div className="flex h-6 items-center">
-              <input
-                name="comments"
-                type="checkbox"
+          <Tooltip
+            content={`Marquee is force-enabled when the text is longer than ${SHORT_LENGTH} characters.`}
+          >
+            <div className="flex flex-row items-center gap-2">
+              <Checkbox
+                id="marquee"
+                disabled={isLong}
                 checked={marqueeSpeed > 0}
                 onChange={(e) => setMarqueeSpeed(e.target.checked ? 5 : 0)}
-                className="h-4 w-4 rounded border-gray-300 text-blue-600 focus:ring-blue-600"
+                className={cx(isLong ? "cursor-not-allowed" : "cursor-pointer")}
               />
+              <Label
+                htmlFor="marquee"
+                disabled={isLong}
+                className={cx(isLong ? "cursor-not-allowed" : "cursor-pointer")}
+              >
+                <span className="font-medium text-gray-900">Marquee</span>{" "}
+                <span className="text-gray-500">makes it sliiiide over.</span>
+              </Label>
             </div>
-            <div className="ml-3 text-sm leading-6">
-              <span className="font-medium text-gray-900">Marquee</span>{" "}
-              <span className="text-gray-500">makes it sliiiide over.</span>
-            </div>
-          </div>
+          </Tooltip>
         </fieldset>
       </div>
 
       <div className="flex flex-col">
-        <button
-          className="rounded-md flex flex-row gap-2 px-4 py-3 items-center justify-center text-sm font-semibold text-white bg-red-600 hover:bg-red-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2"
+        <Button
+          color="failure"
           onClick={() => {
             clearEntries();
           }}
         >
-          <TrashIcon className="h-5 w-5" />
+          <TrashIcon className="mr-2 h-5 w-5" />
           Clear all entries
-        </button>
+        </Button>
       </div>
     </div>
   );
--- dash/tailwind.config.ts
+++ dash/tailwind.config.ts
@@ -3,9 +3,10 @@ import type { Config } from "tailwindcss";
 const config: Config = {
   content: [
     "./src/**/*.{js,ts,jsx,tsx,mdx}",
+    "./node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {},
-  plugins: [require("@tailwindcss/forms")],
+  plugins: [require("@tailwindcss/forms"), require("flowbite/plugin")],
 };
 
 export default config;
