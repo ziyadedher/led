f8ad98f729c997e6e2da2f1e2a30b819c7f6722b
Ziyad Edher <ziyad.edher@gmail.com>
2024-08-15 17:01:43 -0700

    feat(driver-common): add some common utils

---
+++ driver-common/src/config.rs
@@ -0,0 +1,104 @@
+//! Configuration definitions and utilities for the LED driver and associated services.
+//!
+//! This module provides a `Config` struct that defines the configuration for the LED driver and related services like
+//! `led-driverup`. It also provides utilities for saving and loading this configuration to and from a file.
+
+use log;
+use std::fs;
+use std::path::{Path, PathBuf};
+use thiserror::Error;
+use toml;
+
+/// Represents the various errors that can occur during configuration operations.
+///
+/// This enum encapsulates different types of errors that might be encountered when working with configuration files
+/// including I/O errors and config serialization and deserialization errors.
+#[derive(Error, Debug)]
+pub enum Error {
+    /// Represents an I/O error that occurred during file operations.
+    #[error("IO error: {0}")]
+    Io(#[from] std::io::Error),
+
+    /// Represents an error that occurred during config serialization.
+    #[error("Config serialization error: {0}")]
+    Ser(#[from] toml::ser::Error),
+
+    /// Represents an error that occurred during config deserialization.
+    #[error("Config deserialization error: {0}")]
+    De(#[from] toml::de::Error),
+}
+
+/// Configuration for the LED driver and associated services.
+#[derive(serde::Serialize, serde::Deserialize)]
+pub struct Config {
+    /// Unique identifier for this LED matrix.
+    pub id: String,
+
+    /// Path at which to install the LED driver binary.
+    pub install_path: PathBuf,
+
+    /// Path to a directory that will store various log files for the LED driver.
+    pub log_dir: PathBuf,
+}
+
+/// Saves the given configuration to a file at the given path.
+///
+/// Will create the file and directory tree if it doesn't exist, or overwrite it if it does.
+///
+/// # Errors
+///
+/// This function will return a `Error` if the configuration cannot be serialized to TOML or if the file cannot be
+/// written for whatever reason.
+///
+/// # Examples
+///
+/// ```no_run
+/// use led_driver_common::config::{save_config, Config};
+/// use std::path::Path;
+///
+/// # pub fn main() -> Result<(), Error> {
+/// let config = Config {
+///     id: "my-led-matrix".to_string(),
+///     install_path: Path::new("/usr/local/bin/led-driver").to_path_buf(),
+///     log_dir: Path::new("/var/log/led-driver").to_path_buf(),
+/// };
+/// save_config(&config, Path::new("config.toml"))?;
+/// # Ok(())
+/// # }
+/// ```
+pub fn save_config(config: &Config, path: &Path) -> Result<(), Error> {
+    log::debug!("Saving configuration to {:?}...", path);
+    if let Some(parent) = path.parent() {
+        fs::create_dir_all(parent)?;
+    }
+    let config_str = toml::to_string(config)?;
+    fs::write(path, config_str)?;
+    log::debug!("Configuration saved successfully");
+    Ok(())
+}
+
+/// Loads a configuration from a file at the given path.
+///
+/// # Errors
+///
+/// This function will return a `Error` if the file cannot be read, is not valid TOML, or doesn't match the expected
+/// configuration struct ([Config]).
+///
+/// # Examples
+///
+/// ```no_run
+/// use led_driver_common::config::load_config;
+/// use std::path::Path;
+///
+/// # pub fn main() -> Result<(), Error> {
+/// let config = load_config(Path::new("config.toml"))?;
+/// # Ok(())
+/// # }
+/// ```
+pub fn load_config(path: &Path) -> Result<Config, Error> {
+    log::debug!("Loading configuration from {:?}...", path);
+    let config_str = fs::read_to_string(path)?;
+    let config: Config = toml::from_str(&config_str)?;
+    log::debug!("Configuration loaded successfully");
+    Ok(config)
+}
