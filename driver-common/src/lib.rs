2968fafa154a6186218fed5732abdd500a85e27f
Ziyad Edher <ziyad.edher@gmail.com>
2024-08-15 17:01:43 -0700

    feat(driver-common): add some common utils

---
+++ driver-common/src/lib.rs
@@ -0,0 +1,11 @@
+//! `led-driver-common` is a library that provides common functionality for the other LED driver crates
+//!
+//! See more information in the various modules of this crate.
+
+#![warn(missing_docs)]
+#![warn(clippy::all)]
+#![warn(clippy::pedantic)]
+#![warn(clippy::cargo)]
+
+pub mod config;
+pub mod root;
